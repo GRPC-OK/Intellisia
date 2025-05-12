7c665ed83f15a3c18e2195c42691c642bffb9f11
김완기 <dhksrl215@naver.com>
2025-05-12 13:52:46 +0900

    대시보드 페이지 수정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['cdn-icons-png.flaticon.com'],
+  },
 };
 
 export default nextConfig;
