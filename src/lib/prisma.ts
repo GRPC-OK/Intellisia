d7af2e78e11fc74f32c21749e6fc3f607d5e3e72
김완기 <dhksrl215@naver.com>
2025-05-19 18:07:52 +0900

    feat: 공용 DB 연동 및 대시보드 페이지 백엔드 구현

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,8 +8,6 @@ export const prisma =
     log: ['query'],
   });
 
-console.log('DATABASE_URL from env:', process.env.DATABASE_URL); // 데이터베이스가 제대로 연결되었는지 확인하는 코드. 개발 단계에서만 사용 권장!
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
