d2683a7650cccdc8a2cf5575015ae19151094db5
minseonjae <msj6785@naver.com>
2025-05-21 12:56:27 +0900

    next.js 페이지 규칙 위반 수정

---
+++ src/pages/project/[projectId]/version/[versionId]/code-analysis/[issueId].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// ...기존 코드가 있다면 유지...
+
+export default function IssueDetailPage() {
+  return (
+    <div>
+      {/* 상세 내용 또는 추후 구현 */}
+      Issue Detail Page
+    </div>
+  );
+}
