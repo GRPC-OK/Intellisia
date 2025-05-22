ce887821e04faff68551841e10d93596a73172e3
zadkel <45797960+zadkel@users.noreply.github.com>
2025-05-22 13:21:11 +0900

    헤더 버튼 (#88)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 // src/components/Header.tsx
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import { useRouter } from 'next/router';
 
 export default function Header() {
-    return (
-        <nav className={styles.nav}>
-            <div className={styles.logo}>Intellisia🥕</div>
-            <div className={styles.navRight}>
-                <div className={styles.profile}>
-                </div>
-            </div>
-        </nav>
-    )
+  const router = useRouter();
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <button onClick={() => router.push('/dashboard')}>Intellisia🥕</button>
+      </div>
+      <div className={styles.navRight}>
+        <div className={styles.profile}></div>
+      </div>
+    </nav>
+  );
 }
