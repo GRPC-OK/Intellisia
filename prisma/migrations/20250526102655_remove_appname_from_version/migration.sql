d8d8e37b93c9c5070269339e1aaed1b01115de9b
vanillaturtlechips <114771737+vanillaturtlechips@users.noreply.github.com>
2025-05-26 19:48:35 +0900

    fix:prisma (#138)

---
+++ prisma/migrations/20250526102655_remove_appname_from_version/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `applicationName` on the `Version` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Version" DROP COLUMN "applicationName";
